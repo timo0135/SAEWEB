f669402351faac717ab414a87d0c38eacb99436b
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2023-11-06 15:06:09 +0100

    classe de connexion db

---
+++ projet/src/db/ConnectionFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace iutnc\deefy\db;
+
+class ConnectionFactory{
+
+    public static $info = array();
+
+
+
+
+    public static function setConfig($file){
+        ConnectionFactory::$info = parse_ini_file($file);
+    }
+
+    public static function makeConnection(){
+        return new \PDO("mysql:host=".ConnectionFactory::$info['host'].";dbname=".ConnectionFactory::$info['name'], ConnectionFactory::$info['user'], ConnectionFactory::$info['mdp']);
+    }
+
+}
+
