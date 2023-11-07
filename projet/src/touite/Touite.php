8a79b1319d28deed6f0e2b9ea51aa7ed2fed457b
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2023-11-07 09:37:04 +0100

    modif Touite

--- projet/src/touite/Touite.php
+++ projet/src/touite/Touite.php
@@ -13,14 +13,11 @@ class Touite{
     protected ?string $imagePath = null;
     protected ?string $descriptionImage = null;
 
-    public function __construct(User $user, string $texte, string $date, int $answer = null){
+    public function __construct(User $user, string $texte, string $date, int $answer = null, string $imagePath = null, string $descriptionImage = null){
         $this -> user = $user;
         $this -> message = $texte;
         $this -> date = $date;
         $this -> answer = $answer;
-    }
-
-    public function setImage(string $imagePath, string $descriptionImage){
         $this -> imagePath = $imagePath;
         $this -> descriptionImage = $descriptionImage;
     }
@@ -40,13 +37,15 @@ class Touite{
     public function addTouite(){
         ConnectionFactory::setConfig("db.config.ini");
         $bdd = ConnectionFactory::makeConnection();
-        $sql = "insert into touite (message, date, id_user, answer) values (?, ?, ?, ?);";
+        $sql = "insert into touite (message, date, id_user, answer, path, description) values (?, ?, ?, ?, ?, ?);";
         $resultset = $bdd->prepare($sql);
         $resultset->bindParam(1, $this->message);
         $resultset->bindParam(2, $this->date);
         $idUser = $this->user->getIdUser();
         $resultset->bindParam(3, $idUser);
         $resultset->bindParam(4, $this->answer);
+        $resultset->bindParam(5, $this->imagePath);
+        $resultset->bindParam(6, $this->descriptionImage);
         $resultset->execute();
     }
 
