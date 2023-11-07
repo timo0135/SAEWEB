beccd48c7c159724966924d49586ff2e2bca82aa
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-11-07 17:24:07 +0100

    phase de finalisation de RendererTagTouite

--- projet/src/renderer/RendererTagTouite.php
+++ projet/src/renderer/RendererTagTouite.php
@@ -2,6 +2,35 @@
 
 namespace iutnc\deefy\renderer;
 
+use iutnc\deefy\action\ActionDislike;
+use iutnc\deefy\action\ActionLike;
+use iutnc\deefy\db\ConnectionFactory;
+
 class RendererTagTouite
 {
+    private $listTouite;
+    private $resultSet;
+
+    public function __construct($id){
+        $bdd=ConnectionFactory::makeConnection();
+        $this->listTouite = "select * from touite INNER JOIN touite2tag on touite2tag.id_touite=touite.id_touite where id_tag=".$id." and answer is NULL order by date";
+        $this->resultSet = $bdd->prepare($this->listTouite);
+        $this->resultSet->execute();
+    }
+
+    public function render(){
+        $bdd=ConnectionFactory::makeConnection();
+        while ($row=$this->resultSet->fetch()){
+            $user="select firstname, lastname from user where id_user=".$row['id_user'];
+            $res=$bdd->prepare($user);
+            $res->execute();
+            $us=$res->fetch();
+            $affichage="<div><h2>".$us['firsname']." ".$us['lastname']."</h2><br><p>".$row['message']."</p><br>";
+            if(!is_null($row['path'])){
+                $affichage=$affichage."<img src=".$row['path']." alt=".$row['description']."><br>";
+            }
+            $affichage=$affichage."<a href=/index.php?id=".$row['id_touite'].">Voir plus</a></div><br>";
+        }
+        return $affichage;
+    }
 }
