82d80f6fd68b29bd28ffc2278c3d1a377df51128
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-11-07 11:16:35 +0100

    Divers correction du code et ajout RendererTouite

--- projet/src/renderer/RendererTouite.php
+++ projet/src/renderer/RendererTouite.php
@@ -20,6 +20,16 @@ class RendererTouite{
 
     public function render(){
         $bdd=ConnectionFactory::makeConnection();
+        $row=$this->resultSet->fetch();
+        $user="select firstname, lastname from user where id_user=".$row['id_user'];
+        $res=$bdd->prepare($user);
+        $res->execute();
+        $us=$res->fetch();
+        $affichage="<div></div><h2>".$us['firsname']." ".$us['lastname']."</h2><br><p>".$row['message']."</p><br>";
+        if(!is_null($row['path'])){
+            $affichage=$affichage."<img src=".$row['path']."><br>";
+        }
+        $affichage=$affichage."<p>".$row['date']."</p><br><p>".$row['']."</p>";
         while ($row=$this->resultSet->fetch()){
             $user="select firstname, lastname from user where id_user=".$row['id_user'];
             $res=$bdd->prepare($user);
