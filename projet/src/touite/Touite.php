a05b3c0474dacae983b90c851c07549a024f23f3
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2023-11-07 09:31:17 +0100

    Supression de la classe commentaire et modif classe touite

--- projet/src/touite/Touite.php
+++ projet/src/touite/Touite.php
@@ -8,13 +8,16 @@ class Touite{
     protected User $user;
     protected string $message;
     protected string $date;
+
+    protected ?int $answer = null;
     protected ?string $imagePath = null;
     protected ?string $descriptionImage = null;
 
-    public function __construct(User $user, string $texte, string $date){
+    public function __construct(User $user, string $texte, string $date, int $answer = null){
         $this -> user = $user;
         $this -> message = $texte;
         $this -> date = $date;
+        $this -> answer = $answer;
     }
 
     public function setImage(string $imagePath, string $descriptionImage){
@@ -37,12 +40,13 @@ class Touite{
     public function addTouite(){
         ConnectionFactory::setConfig("db.config.ini");
         $bdd = ConnectionFactory::makeConnection();
-        $sql = "insert into touite (message, date, id_user) values (?, ?, ?);";
+        $sql = "insert into touite (message, date, id_user, answer) values (?, ?, ?, ?);";
         $resultset = $bdd->prepare($sql);
         $resultset->bindParam(1, $this->message);
         $resultset->bindParam(2, $this->date);
         $idUser = $this->user->getIdUser();
         $resultset->bindParam(3, $idUser);
+        $resultset->bindParam(4, $this->answer);
         $resultset->execute();
     }
 
