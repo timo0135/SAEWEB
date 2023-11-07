798b604be81130cc60068fc00b8027c0c44794e1
Hugo Rysak <hugo.rysak4@etu.univ-lorraine.fr>
2023-11-07 15:29:45 +0100

    ajout de l'action pour s'abboner ou se desabboner

---
+++ projet/src/action/ActionSubscribe.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace iutnc\deefy\action;
+
+use Cassandra\PreparedStatement;
+use iutnc\deefy\db\ConnectionFactory;
+
+class ActionSubscribe extends Action
+{
+
+    public function execute(): string
+    {
+        $bdo=ConnectionFactory::makeConnection();
+        $sql="SELECT publisher from subscribe where subscriber=".$_SESSION['id'];
+        if($resultSet=$bdo->query($sql)){
+            $sql="DELETE FROM SUBSCRIBE WHERE publisher=? and subrsciber=?";
+            $resultSet2=$bdo->prepare($sql);
+            $resultSet2->bindParam(1,$_GET['id']);
+            $resultSet2->bindParam(2,$_SESSION['id']);
+            $resultSet2->execute();
+            $res="Vous vous êtes bien desabonnées";
+        }else{
+        $sql="INSERT INTO SUBSCRIBE VALUES(?,?)";
+        $resultSet2=$bdo->prepare($sql);
+        $resultSet2->bindParam(1,$_GET['id'],$_SESSION['id']);
+        $resultSet2->execute();
+        $res="Vous vous êtes bien abonné";
+        }
+        return $res;
+    }
+}
