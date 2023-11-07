5fa96469a8c475f35eb50589f92c61da59a59b51
Hugo Rysak <hugo.rysak4@etu.univ-lorraine.fr>
2023-11-07 08:59:54 +0100

    modif classe user

--- projet/src/user/User.php
+++ projet/src/user/User.php
@@ -5,15 +5,16 @@ namespace iutnc\deefy\user;
 class User
 {
     private int $id_user,$role;
-    private string $email,$firstname,$lastname;
+    private string $email,$firstname,$lastname,$pwd;
 
 
-    private function __construct(int $id,int $role,string $email,string $firstname,string $lastname){
+    private function __construct(int $id,int $role,string $email,string $firstname,string $lastname, string $pwd){
         $this->id_user=$id;
         $this->role=$role;
         $this->email=$email;
         $this->firstname=$firstname;
         $this->lastname=$lastname;
+        $this->pwd=$pwd;
     }
 
 }
