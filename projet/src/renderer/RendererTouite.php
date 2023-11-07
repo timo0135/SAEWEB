0b729bedff529ffabb3e1c444dd74f80238735cf
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-11-07 11:34:46 +0100

    Divers correction du code et amelioration de RendererTouite

--- projet/src/renderer/RendererTouite.php
+++ projet/src/renderer/RendererTouite.php
@@ -10,12 +10,17 @@ class RendererTouite{
 
     private $touite;
     private $resultSet;
+    private $commentaire;
+    private $resCom;
 
     public function __construct(int $id){
         $bdd=ConnectionFactory::makeConnection();
         $this->touite = "select * from touite where id_touite=".$id;
         $this->resultSet = $bdd->prepare($this->touite);
         $this->resultSet->execute();
+        $this->commentaire="select * from touite where answer=".$id;
+        $this->resCom = $bdd->prepare($this->commentaire);
+        $this->resCom->execute();
     }
 
     public function render(){
@@ -25,12 +30,17 @@ class RendererTouite{
         $res=$bdd->prepare($user);
         $res->execute();
         $us=$res->fetch();
+
+
         $affichage="<div></div><h2>".$us['firsname']." ".$us['lastname']."</h2><br><p>".$row['message']."</p><br>";
         if(!is_null($row['path'])){
             $affichage=$affichage."<img src=".$row['path']."><br>";
         }
-        $affichage=$affichage."<p>".$row['date']."</p><br><p>".$row['']."</p>";
-        while ($row=$this->resultSet->fetch()){
+        $affichage=$affichage."<p>".$row['date']."</p><br><p>".$row['description']."</p>";
+
+
+
+        while ($row=$this->resCom->fetch()){
             $user="select firstname, lastname from user where id_user=".$row['id_user'];
             $res=$bdd->prepare($user);
             $res->execute();
