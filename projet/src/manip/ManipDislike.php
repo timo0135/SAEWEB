3370abd41b1061656116878b98d2575def35c0d4
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-11-08 14:32:15 +0100

    Correction erreur

--- projet/src/manip/ManipDislike.php
+++ projet/src/manip/ManipDislike.php
@@ -9,18 +9,17 @@ class ManipDislike
 {
     public function execute(): void
     {
-        session_start();
         $res="";
         $bdd = ConnectionFactory::makeConnection();
         $sql = "select * from like where id_touite=" . $_GET['id'] . " and id_user=" . $_SESSION['id'];
         $resultSet = $bdd->prepare($sql);
         $resultSet->execute();
         if ($resultSet->fetch()) {
-            $sql = "update like set like=false";
+            $sql = "update like set like=0";
             $resultSet = $bdd->prepare($sql);
             $resultSet->execute();
         } else {
-            $sql = "INSERT INTO like VALUES(?,?,false)";
+            $sql = "INSERT INTO like VALUES(?,?,0)";
             $resultSet->bindParam(1, $_SESSION['id']);
             $resultSet->bindParam(2, $_GET['id']);
             $resultSet = $bdd->prepare($sql);
--- projet/src/manip/ManipLike.php
+++ projet/src/manip/ManipLike.php
@@ -9,18 +9,17 @@ class ManipLike
 {
     public function execute(): void
     {
-        session_start();
         $res="";
         $bdd = ConnectionFactory::makeConnection();
         $sql = "select * from like where id_touite=" . $_GET['id'] . " and id_user=" . $_SESSION['id'];
         $resultSet = $bdd->prepare($sql);
         $resultSet->execute();
         if ($resultSet->fetch()) {
-            $sql = "update like set like=true";
+            $sql = "update like set like=1";
             $resultSet = $bdd->prepare($sql);
             $resultSet->execute();
         } else {
-            $sql = "INSERT INTO like VALUES(?,?,true)";
+            $sql = "INSERT INTO like VALUES(?,?,1)";
             $resultSet->bindParam(1, $_SESSION['id']);
             $resultSet->bindParam(2, $_GET['id']);
             $resultSet = $bdd->prepare($sql);
