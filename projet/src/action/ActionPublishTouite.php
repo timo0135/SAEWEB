87e0a3c6755b533172a765b21853b74f30e1cf69
Hugo Rysak <hugo.rysak4@etu.univ-lorraine.fr>
2023-11-10 15:01:15 +0100

    commentaire plus erreur

--- projet/src/action/ActionPublishTouite.php
+++ projet/src/action/ActionPublishTouite.php
@@ -13,6 +13,7 @@ class ActionPublishTouite extends Action
 
     public function execute(): string
     {
+        //Si la methode est get On demande à l'utilisateur les information pour publier son touitte
         if ($this->http_method==="GET"){
             $res= "
             <fieldset class='form-fit'><legend><h1>Publier un touite</h1></legend>
@@ -26,12 +27,15 @@ class ActionPublishTouite extends Action
                 <input class='envoyer' type='submit' name='envoyer' value='Touitter'><br>
                 </form>
              </fieldset>";
-        }else if ($this->http_method==="POST"){
+        }else if ($this->http_method==="POST"){//sinon on doit psoter le touitte sur la base de donée
            try {
+               //on utilise la methode add_touite de Maniptouite pour uploader le touite dans la base de donnée
                 ManipTouite::add_touite();
+                //si tout se passe bien on redirige l'utilisateur vers le menu
                 header('location:index.php');
           }catch (\Exception $e){
-              // unset($_FILES['image']);
+              //si la methode add_touite de ManipTouite renvoie une exception (qui est forcément lié au fichier que l'utilisateur essaye d'uploader
+               //on lui renvoie le formulaire en lui signalant l'erreur
                 $res= "
                 <fieldset class='form-fit'><legend><h1>Publier un touite</h1></legend>
                 <form method='post' name='Ajouter Touitte' enctype='multipart/form-data'>
@@ -48,7 +52,7 @@ class ActionPublishTouite extends Action
                 return $res;
             }
         }
-        //unset($_FILES['image']);
+
         return $res;
     }
 }
--- projet/src/action/ActionRechercherTag.php
+++ projet/src/action/ActionRechercherTag.php
@@ -10,14 +10,17 @@ class ActionRechercherTag extends Action
     public function execute(): string
     {
         $res="";
+        //On selectionne toute les informations d'une tag qui ressemble au mit entré par l'utilisateur
         $sql="SELECT id_tag,label,description from tag where label like ?";
         $bdd=ConnectionFactory::makeConnection();
         $resultSet=$bdd->prepare($sql);
         $str = $_POST['recherche'] . "%";
         $resultSet->bindParam(1, $str);
         $resultSet->execute();
+        //on afficher chaque tag retourner par la requête
         while ($row=$resultSet->fetch()){
             $id_tag=$row['id_tag'];
+            //on regarde si l'utilisateur est abonné au tag que l'on va afficher
             $sql2="select * from user2tag where id_tag=? and id_user=?";
 
             $resultSet2=$bdd->prepare($sql2);
@@ -25,16 +28,21 @@ class ActionRechercherTag extends Action
             $resultSet2->bindParam(2,$_SESSION['id']);
             $resultSet2->execute();
             $abonnement="S abonner";
+            //si il est deja abonné au tag le bouton va afficher Se désabonner au lieu de s'abonner
             if($resultSet2->fetch()){
                 $abonnement="Se désabonner";
             }
+            //on met un lien sur le nom du tag qui envoie vers la page du tag et on affiche le tag
             $res.="<div class =tag>
                 <a href='index.php?action=page-tag&id_tag=".$row['id_tag']."'><h4 class='labeltag'>".$row['label']."</h4></a><br>";
+            //si il y a une description on l'affiche
             if(!is_null($row['description'])){
                 $res.="<p>".$row['description']."</p><br>";
             }else{
+                //sinon pas de description
                 $res.="<p>Pas de description</p><br>";
             }
+            //bouton pour s'abonner ou se desabonner d'un tag
             $res.="<form  method='post' action='index.php?action=subscribeTag&id_tag=$id_tag'>
                 <input class='abb' type='submit' name='subscribeTag' value='$abonnement'><br>
                 </form>";
