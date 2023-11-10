681114f08c5eb276a740845aa9e318d3cda1e1c5
unknown <maxime.bechard7@etu.univ-lorraine.fr>
2023-11-10 13:53:18 +0100

    remplace voireplus avec un icon

--- backoffice/index.php
+++ backoffice/index.php
@@ -32,13 +32,39 @@ ConnectionFactory::setConfig("db.config.ini");
 $bddPDO = ConnectionFactory::makeConnection();
 
 
-$commande="SELECT tag.id_tag AS id,tag.label AS lb,count(*) AS nb FROM tag JOIN touite2tag ON touite2tag.id_tag = tag.id_tag GROUP BY tag.id_tag ORDER BY count(*) DESC";
+if(isset($_GET['pagetag'])){
+    $pagetag=(int) $_GET['pagetag'];
+}else{
+    $pagetag= 1;
+}
+
+if(isset($_GET['pageuser'])){
+    $pageuser=(int) $_GET['pageuser'];
+}else{
+    $pageuser= 1;
+}
+
+
+
+$commande="SELECT tag.id_tag AS id,tag.label AS lb,count(*) AS nb FROM tag JOIN touite2tag ON touite2tag.id_tag = tag.id_tag GROUP BY tag.id_tag ORDER BY count(*) DESC LIMIT 10 OFFSET ".(($pagetag-1)*10).";";
 $i=1;
 $result=$bddPDO->query($commande);
 while($row = $result->fetch()){
     echo "<a class='lst-tag' href='../projet/index.php?action=page-tag&id_tag=".$row['id']."'>$i- ".$row['lb']."&nbsp(".$row['nb'].")</a><br>";
     $i++;
 }
+echo "<div class='container'>";
+if($pagetag != 1){
+    echo "
+        <a href='index.php?pageuser=".$pageuser."&pagetag=".($pagetag-1)."'>precedent</a>
+    ";
+}
+if($i == 11){
+    echo "
+        <a href='index.php?pageuser=".$pageuser."&pagetag=".($pagetag+1)."'>suivant</a>
+    ";
+}
+echo "</div>";
 ?>
         </div>
         <div class='box-fit'>
@@ -51,13 +77,25 @@ ConnectionFactory::setConfig("db.config.ini");
 $bddPDO = ConnectionFactory::makeConnection();
 
 
-$commande="SELECT id_user,email,count(*) AS nb FROM User Join subsribe ON User.id_user = subsribe.publisher GROUP BY email ORDER BY count(*) DESC";
+$commande="SELECT id_user,email,count(*) AS nb FROM User Join subsribe ON User.id_user = subsribe.publisher GROUP BY email ORDER BY count(*) DESC LIMIT 10 OFFSET ".(($pageuser-1)*10).";";
 $i=1;
 $result=$bddPDO->query($commande);
 while($row = $result->fetch()){
     echo "<a class='lst-tag' href='../projet/index.php?action=page-user&id_user=".$row['id_user']."'>$i- ".$row['email']."&nbsp(".$row['nb'].")</a><br>";
     $i++;
 }
+echo "<div class='container'>";
+if($pageuser != 1){
+    echo "
+        <a href='index.php?pageuser=".($pageuser-1)."&pagetag=".$pagetag."'>precedent</a>
+    ";
+}
+if($i == 11){
+    echo "
+        <a href='index.php?pageuser=".($pageuser+1)."&pagetag=".$pagetag."'>precedent</a>
+    ";
+}
+echo "</div>";
 ?>
         </div>
     </div>
